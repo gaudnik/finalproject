8ba487c1aad861ec737aa306592e3e60993aa9d6
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-06-20 09:30:26 +0200

    add states

--- src/Components/Quiz.js
+++ src/Components/Quiz.js
@@ -3,6 +3,12 @@ import {questions} from './Questions';
 
 
 const Quiz = () => {
+ const [currentQuestion, setCurrentQuestion] = useState(0);
+ const [myAnswer, setMyAnswer] = useState("");
+ const [score, setScore] = useState(0);
+ const [finish, setFinish] = useState(false);
+ const [show, setShow] = useState(false);
+ const [clickAnswer, setClickAnswer] = useState(false);
 }
 
 
