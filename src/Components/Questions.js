404339bed997d1ff23874b4ee3ec82e07cc4dda0
ZuzannaGaudnik <zuzanna.woda@gmail.com>
2022-06-20 12:52:56 +0200

    add questions

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -12,10 +12,101 @@
     },
     {
         "id": 1,
-        "question": "",
-        "answer": "",
+        "question": "What creature is Aragog?",
+        "answer": "Acromantula",
         "variants": [
-
+            "Acromantula",
+            "Dog",
+            "Monkey",
+            "Bird"
         ]
-    }
+    },
+     {
+         "id": 2,
+         "question": "What was Hagrid's pet dog called?",
+         "answer": "Fang",
+         "variants": [
+             "Dean",
+             "Elzo",
+             "Fang",
+             "Max"
+         ]
+     },
+     {
+         "id": 3,
+         "question": "Name the centaur who taught at Hogwarts.",
+         "answer": "Firenze",
+         "variants": [
+             "Bane",
+             "Firenze",
+             "Ronan",
+             "Magorian"
+         ]
+     },
+     {
+         "id": 4,
+         "question": "how many people started hogwarts?",
+         "answer": "4",
+         "variants": [
+             "4",
+             "2",
+             "6",
+             "9"
+         ]
+     },
+     {
+         "id": 5,
+         "question": "... Granger",
+         "answer": "Hermione",
+         "variants": [
+             "Lavender",
+             "Hermione",
+             "Luna",
+             "Elize"
+         ]
+     },
+     {
+         "id": 6,
+         "question": "How many brothers does Ronald Wesley have?",
+         "answer": "5",
+         "variants": [
+             "3",
+             "1",
+             "9",
+             "5"
+         ]
+     },
+     {
+         "id": 7,
+         "question": "who is the publisher of the \"Juggler\"?",
+         "answer": "Xenophilius Lovegood",
+         "variants": [
+             "Lucius Malfoy",
+             "Cornelius Fudge",
+             "Arthur Wesley",
+             "Xenophilius Lovegood"
+         ]
+     },
+     {
+         "id": 8,
+         "question": "Who is Harry's best friend?",
+         "answer": "Ron Wesley",
+         "variants": [
+             "Neville Longbottom",
+             "Dean Thomas",
+             "Ron Wesley",
+             "Draco Malfoy"
+         ]
+     },
+     {
+         "id": 9,
+         "question": "What was the name of Lord Voldemort's loyal snake?\n",
+         "answer": "Nagini",
+         "variants": [
+             "Nagini",
+             "Ellen",
+             "Morgan",
+             "Nina"
+         ]
+     }
  ];
